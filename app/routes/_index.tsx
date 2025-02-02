bb08ff32e745fb05cd43c6ab7126a61a550cf1c1
Rony <villedarony91@gmail.com>
2025-02-01 18:37:11 -0600

    feat: add tailwind conf

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,7 +9,7 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+    <div className="bg-blue-800 p-4" style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
       <ul>
         <li>

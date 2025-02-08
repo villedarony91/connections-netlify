f94703602acf3c779f518a1e5b1c6da09661d8a2
Rony <villedarony91@gmail.com>
2025-02-07 19:58:46 -0600

    feat: add navbar

--- app/root.tsx
+++ app/root.tsx
@@ -7,12 +7,55 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import { Disclosure, DisclosureButton } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { PlusIcon } from '@heroicons/react/20/solid'
+
 import stylesheet from "~/tailwind.css?url";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
 ];
 
+const TopBar = () => {
+  return (
+    <Disclosure as="nav" className="bg-primary-green shadow">
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="flex h-16 justify-between">
+        <div className="flex">
+          <div className="-ml-2 mr-2 flex items-center md:hidden">
+            {/* Mobile menu button */}
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+              <span className="absolute -inset-0.5" />
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon aria-hidden="true" className="block size-6 group-data-[open]:hidden" />
+              <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-[open]:block" />
+            </DisclosureButton>
+          </div>
+          <div className="flex shrink-0 items-center">
+            <img
+              alt="Your Company"
+              src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+              className="h-8 w-auto"
+            />
+          </div>
+        </div>
+        <div className="flex items-center">
+          <div className="shrink-0">
+            <button
+              type="button"
+              className="relative inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              <PlusIcon aria-hidden="true" className="-ml-0.5 size-5" />
+              Contactanos
+            </button>
+          </div>           
+        </div>
+      </div>
+    </div>
+  </Disclosure>
+  )
+}
 export default function App() {
   return (
     <html lang="en">
@@ -26,6 +69,7 @@ export default function App() {
         <Outlet />
         <ScrollRestoration />
         <Scripts />
+        <TopBar />
       </body>
     </html>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import type { Config } from 'tailwindcss'
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-green': '#093509',
+        'secondary-yellow': '#f6d44d',
+      }
+    },
   },
   plugins: [],
 } satisfies Config

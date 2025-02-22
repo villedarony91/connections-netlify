6dfade0405c161f7d0bd27bd7b80a700919c4741
Rony <villedarony91@gmail.com>
2025-02-22 10:21:34 -0600

    feat: add contact button with icon

--- app/root.tsx
+++ app/root.tsx
@@ -8,7 +8,7 @@ import {
 } from "@remix-run/react";
 
 import { Disclosure, DisclosureButton } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon, PhoneIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { PlusIcon } from '@heroicons/react/20/solid'
 
 import stylesheet from "~/tailwind.css?url";
@@ -24,27 +24,18 @@ const TopBar = () => {
     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div className="flex h-16 justify-between">
         <div className="flex">
-          <div className="-ml-2 mr-2 flex items-center md:hidden">
-            {/* Mobile menu button */}
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-              <span className="absolute -inset-0.5" />
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon aria-hidden="true" className="block size-6 group-data-[open]:hidden" />
-              <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-[open]:block" />
-            </DisclosureButton>
-          </div>
             <LogoSvg className="fill-secondary-yellow"/>
         </div>
         <div className="flex items-center">
           <div className="shrink-0">
             <button
               type="button"
-              className="relative inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="relative inline-flex items-center gap-x-1.5 px-3 py-2 text-sm font-semibold focus-visible:outline-2 focus-visible:outline-offset-2 text-secondary-yellow"
             >
-              <PlusIcon aria-hidden="true" className="-ml-0.5 size-5" />
-              Contactanos
+              <PhoneIcon aria-hidden="true" className="-ml-0.5 size-5" />
+              Cont&aacute;ctanos
             </button>
-          </div>           
+          </div>
         </div>
       </div>
     </div>
